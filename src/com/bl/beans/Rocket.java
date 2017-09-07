b724b8d490cc43bcb3a8b6ce37b06d3d1028a6c1
Neeaj Kumar <neeraj170291.kumar@gmail.com>
2017-09-07 18:29:57 +0530

    First Commit

---
+++ src/com/bl/beans/Rocket.java
@@ -0,0 +1,42 @@
+package com.bl.beans;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+
+public class Rocket implements BeanFactoryAware {
+	private int threshold;
+	private int level;
+	private double distance;
+	
+	public Rocket(int threshold) {
+		System.out.println("Rocket(threshold)");
+		this.threshold = threshold;
+		
+	}
+	
+	public void setLevel(int level) {
+		System.out.println("setLevel(level)");
+		this.level = level;
+	}
+
+	@Override
+	public void setBeanFactory(BeanFactory factory) throws BeansException {
+		System.out.println("setBeanFactory()");
+	}
+	
+	public void prepare() {
+		System.out.println("prepare()");
+		this.distance = this.threshold * this.level;
+	}
+	
+	public void release() {
+		System.out.println("release()");
+	}
+
+	public String toString() {
+		return "Rocket [threshold=" + threshold + ", level=" + level
+				+ ", distance=" + distance + "]";
+	}
+	
+}
